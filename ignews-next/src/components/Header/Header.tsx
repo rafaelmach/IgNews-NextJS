a75f4f3b64369aa0322cfe78ebde2b4be423873f
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2022-05-10 16:56:06 -0300

    Componente Header

---
+++ ignews-next/src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import styles from "./styles.module.scss"
+
+export const Header = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Ig.News" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+      </div>
+    </header>
+  )
+}
+
